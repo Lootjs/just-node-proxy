5865366f550d25e7da37adfd98e523ccb3fd983f
Loot Nurakhmetov <nurakhmetov.l@rahmetapp.kz>
2024-01-13 19:28:27 +0600

    init

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const cors = require('cors') 
+
+const app = express();
+const port = 3000;
+
+const LOKALISE_API_ENDPOINT = 'https://api.lokalise.com/api2';
+
+app.use(express.json());
+app.use(cors()); 
+
+app.all('*', async (req, res) => {
+    const url = LOKALISE_API_ENDPOINT + req.url;
+    try {
+        if (req.method === 'GET') {
+            const response = await fetch(url, {
+                method: req.method,
+                headers: {
+                    'X-Api-Token': req.headers['x-api-token']
+                },
+            });
+            const data = await response.json();
+            res.send(data);
+        } else {
+            const response = await fetch(url, {
+                method: req.method,
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-Api-Token': req.headers['X-Api-Token']
+                },
+                body: JSON.stringify(req.body)
+            });
+            const data = await response.json();
+            res.send(data);
+        }
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Listening http://localhost:${port}`);
+});
